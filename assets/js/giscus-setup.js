3799025d37422d7099596d08a288b1db90a7a588
exclamative <elena.castroviejo@gmail.com>
2025-10-31 20:05:15 +0100

    Deploy site

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "exclamative/exclamative.github.io",
+    "data-repo": "",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
